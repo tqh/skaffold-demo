6a14c10787685818e05737004a8438cfa6aa9d65
tqh <fredrik.holmqvist@gmail.com>
2024-01-28 20:21:01 +0100

    First version of simple web app

---
+++ app/cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+)
+
+var (
+	counter   = make(map[string]int64)
+	countLock sync.Mutex
+)
+
+func count(name string) int64 {
+	countLock.Lock()
+	defer countLock.Unlock()
+	counter[name] += 1
+	return counter[name]
+}
+
+func nameFromPath(path string) string {
+	path = strings.TrimPrefix(path, "/")
+	return html.EscapeString(path)
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		name := nameFromPath(r.URL.Path)
+		_, _ = fmt.Fprintf(w, "Hi, %q: %d", name, count(name))
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ app/go.mod
@@ -0,0 +1,3 @@
+module github.com/tqh/skaffold-demo/app
+
+go 1.22.0
